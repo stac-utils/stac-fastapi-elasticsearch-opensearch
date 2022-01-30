349e57274d738a33fca145f66bc436b0ec22902c
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-30 05:20:14 -0800

    work mongo

--- stac_fastapi/mongo/setup.py
+++ stac_fastapi/mongo/setup.py
@@ -9,11 +9,12 @@ install_requires = [
     "attrs",
     "pydantic[dotenv]",
     "stac_pydantic==2.0.*",
-    "stac-fastapi.types",
-    "stac-fastapi.api",
-    "stac-fastapi.extensions",
+    "stac-fastapi.types==2.3.0",
+    "stac-fastapi.api==2.3.0",
+    "stac-fastapi.extensions==2.3.0",
     "fastapi-utils",
     "pymongo",
+    "pystac[validation]"
 ]
 
 extra_reqs = {
