375d7965ca1a705c4ad63b57612a4ca0bdfa24b5
jonhealy1 <jonathan.d.healy@gmail.com>
2022-01-30 05:36:46 -0800

    add elasticsearch folder

---
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
@@ -0,0 +1,76 @@
+"""FastAPI application."""
+from stac_fastapi.api.app import StacApi
+from stac_fastapi.api.models import create_get_request_model, create_post_request_model
+from stac_fastapi.elasticsearch.config import ElasticsearchSettings
+from stac_fastapi.elasticsearch.core import CoreCrudClient
+from stac_fastapi.elasticsearch.extensions import QueryExtension
+from stac_fastapi.elasticsearch.session import Session
+from stac_fastapi.elasticsearch.transactions import (
+    BulkTransactionsClient,
+    TransactionsClient,
+)
+from stac_fastapi.extensions.core import (
+    ContextExtension,
+    FieldsExtension,
+    SortExtension,
+    TokenPaginationExtension,
+    TransactionExtension,
+)
+from stac_fastapi.extensions.third_party import BulkTransactionExtension
+
+settings = ElasticsearchSettings()
+session = Session.create_from_settings(settings)
+
+extensions = [
+    TransactionExtension(client=TransactionsClient(session=session), settings=settings),
+    BulkTransactionExtension(client=BulkTransactionsClient(session=session)),
+    FieldsExtension(),
+    QueryExtension(),
+    SortExtension(),
+    TokenPaginationExtension(),
+    ContextExtension(),
+]
+
+post_request_model = create_post_request_model(extensions)
+
+api = StacApi(
+    settings=settings,
+    extensions=extensions,
+    client=CoreCrudClient(session=session, post_request_model=post_request_model),
+    search_get_request_model=create_get_request_model(extensions),
+    search_post_request_model=post_request_model,
+)
+app = api.app
+
+
+def run():
+    """Run app from command line using uvicorn if available."""
+    try:
+        import uvicorn
+
+        uvicorn.run(
+            "stac_fastapi.elasticsearch.app:app",
+            host=settings.app_host,
+            port=settings.app_port,
+            log_level="info",
+            reload=settings.reload,
+        )
+    except ImportError:
+        raise RuntimeError("Uvicorn must be installed in order to use command")
+
+
+if __name__ == "__main__":
+    run()
+
+
+def create_handler(app):
+    """Create a handler to use with AWS Lambda if mangum available."""
+    try:
+        from mangum import Mangum
+
+        return Mangum(app)
+    except ImportError:
+        return None
+
+
+handler = create_handler(app)
---
+++ stac_fastapi/elasticsearch/tests/conftest.py
@@ -0,0 +1,156 @@
+import json
+import os
+from typing import Callable, Dict
+
+import pytest
+from starlette.testclient import TestClient
+
+from stac_fastapi.api.app import StacApi
+from stac_fastapi.api.models import create_request_model
+from stac_fastapi.elasticsearch.config import ElasticsearchSettings
+from stac_fastapi.elasticsearch.core import CoreCrudClient
+from stac_fastapi.elasticsearch.extensions import QueryExtension
+from stac_fastapi.elasticsearch.transactions import (
+    BulkTransactionsClient,
+    TransactionsClient,
+)
+from stac_fastapi.extensions.core import (
+    ContextExtension,
+    FieldsExtension,
+    SortExtension,
+    TokenPaginationExtension,
+    TransactionExtension,
+)
+from stac_fastapi.types.config import Settings
+from stac_fastapi.types.errors import ConflictError
+from stac_fastapi.types.search import BaseSearchGetRequest, BaseSearchPostRequest
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+
+class TestSettings(ElasticsearchSettings):
+    class Config:
+        env_file = ".env.test"
+
+
+settings = TestSettings()
+Settings.set(settings)
+
+
+@pytest.fixture(autouse=True)
+def cleanup(es_core: CoreCrudClient, es_transactions: TransactionsClient):
+    yield
+    collections = es_core.all_collections(request=MockStarletteRequest)
+    for coll in collections["collections"]:
+        if coll["id"].split("-")[0] == "test":
+            # Delete the items
+            items = es_core.item_collection(
+                coll["id"], limit=100, request=MockStarletteRequest
+            )
+            for feat in items["features"]:
+                try:
+                    es_transactions.delete_item(
+                        feat["id"], feat["collection"], request=MockStarletteRequest
+                    )
+                except Exception:
+                    pass
+
+            # Delete the collection
+            try:
+                es_transactions.delete_collection(
+                    coll["id"], request=MockStarletteRequest
+                )
+            except Exception:
+                pass
+
+
+@pytest.fixture
+def load_test_data() -> Callable[[str], Dict]:
+    def load_file(filename: str) -> Dict:
+        with open(os.path.join(DATA_DIR, filename)) as file:
+            return json.load(file)
+
+    return load_file
+
+
+class MockStarletteRequest:
+    base_url = "http://test-server"
+
+
+# @pytest.fixture
+# def db_session() -> Session:
+#     return Session(
+#         reader_conn_string=settings.reader_connection_string,
+#         writer_conn_string=settings.writer_connection_string,
+#     )
+
+
+@pytest.fixture
+def es_core():
+    return CoreCrudClient(session=None)
+
+
+@pytest.fixture
+def es_transactions():
+    return TransactionsClient(session=None)
+
+
+@pytest.fixture
+def es_bulk_transactions():
+    return BulkTransactionsClient(session=None)
+
+
+@pytest.fixture
+def api_client():
+    settings = ElasticsearchSettings()
+    extensions = [
+        TransactionExtension(
+            client=TransactionsClient(session=None), settings=settings
+        ),
+        ContextExtension(),
+        SortExtension(),
+        FieldsExtension(),
+        QueryExtension(),
+        TokenPaginationExtension(),
+    ]
+
+    get_request_model = create_request_model(
+        "SearchGetRequest",
+        base_model=BaseSearchGetRequest,
+        extensions=extensions,
+        request_type="GET",
+    )
+
+    post_request_model = create_request_model(
+        "SearchPostRequest",
+        base_model=BaseSearchPostRequest,
+        extensions=extensions,
+        request_type="POST",
+    )
+
+    return StacApi(
+        settings=settings,
+        client=CoreCrudClient(
+            session=None,
+            extensions=extensions,
+            post_request_model=post_request_model,
+        ),
+        extensions=extensions,
+        search_get_request_model=get_request_model,
+        search_post_request_model=post_request_model,
+    )
+
+
+@pytest.fixture
+def app_client(api_client, load_test_data):
+    coll = load_test_data("test_collection.json")
+    client = TransactionsClient(
+        session=None,
+    )
+    try:
+        client.create_collection(coll, request=MockStarletteRequest)
+    except ConflictError:
+        pass
+
+    with TestClient(api_client.app) as test_app:
+        yield test_app
