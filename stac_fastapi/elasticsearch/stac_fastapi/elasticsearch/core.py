1262e89db2063c4ca0a467ab209ba4b2e2171995
jonhealy1 <jonathan.d.healy@gmail.com>
2022-03-01 00:13:11 -0800

    fix item collection, single item error messages

--- stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/core.py
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/core.py
@@ -102,15 +102,16 @@ class CoreCrudClient(BaseCoreClient):
         """Read an item collection from the database."""
         links = []
         base_url = str(kwargs["request"].base_url)
-
         search = Search(using=self.client, index="stac_items")
 
         collection_filter = Q(
             "bool", should=[Q("match_phrase", **{"collection": collection_id})]
         )
         search = search.query(collection_filter)
-
-        count = search.count()
+        try:
+            count = search.count()
+        except elasticsearch.exceptions.NotFoundError:
+            raise NotFoundError(f"No items exist for this collection yet")
         # search = search.sort({"id.keyword" : {"order" : "asc"}})
         search = search.query()[0:limit]
         collection_children = search.execute().to_dict()
@@ -141,7 +142,7 @@ class CoreCrudClient(BaseCoreClient):
         try:
             item = self.client.get(index="stac_items", id=item_id)
         except elasticsearch.exceptions.NotFoundError:
-            raise NotFoundError
+            raise NotFoundError(f"Item {item_id} does not exist in Collection {collection_id}")
         return self.item_serializer.db_to_stac(item["_source"], base_url)
 
     def _return_date(self, datetime):
