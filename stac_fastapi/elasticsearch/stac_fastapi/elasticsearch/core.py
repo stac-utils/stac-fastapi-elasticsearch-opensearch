02b896a9812f831c4f61fd6d5dd1fb088da9e5c4
Stijn Caerts <stijn.caerts@vito.be>
2023-12-05 09:53:17 +0100

    upgrade stac-fastapi to v2.4.9, implement support for BulkTransaction method parameter

--- stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/core.py
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/core.py
@@ -26,7 +26,7 @@ from stac_fastapi.elasticsearch.serializers import CollectionSerializer, ItemSer
 from stac_fastapi.elasticsearch.session import Session
 from stac_fastapi.extensions.third_party.bulk_transactions import (
     BaseBulkTransactionsClient,
-    Items,
+    Items, BulkTransactionMethod,
 )
 from stac_fastapi.types import stac as stac_types
 from stac_fastapi.types.config import Settings
@@ -568,7 +568,7 @@ class TransactionsClient(AsyncBaseTransactionsClient):
         if item["type"] == "FeatureCollection":
             bulk_client = BulkTransactionsClient()
             processed_items = [
-                bulk_client.preprocess_item(item, base_url) for item in item["features"]  # type: ignore
+                bulk_client.preprocess_item(item, base_url, BulkTransactionMethod.INSERT) for item in item["features"]  # type: ignore
             ]
 
             await self.database.bulk_async(
@@ -718,17 +718,19 @@ class BulkTransactionsClient(BaseBulkTransactionsClient):
         settings = ElasticsearchSettings()
         self.client = settings.create_client
 
-    def preprocess_item(self, item: stac_types.Item, base_url) -> stac_types.Item:
+    def preprocess_item(self, item: stac_types.Item, base_url, method: BulkTransactionMethod) -> stac_types.Item:
         """Preprocess an item to match the data model.
 
         Args:
             item: The item to preprocess.
             base_url: The base URL of the request.
+            method: The bulk transaction method.
 
         Returns:
             The preprocessed item.
         """
-        return self.database.sync_prep_create_item(item=item, base_url=base_url)
+        exist_ok = (method == BulkTransactionMethod.UPSERT)
+        return self.database.sync_prep_create_item(item=item, base_url=base_url, exist_ok=exist_ok)
 
     @overrides
     def bulk_item_insert(
@@ -751,7 +753,7 @@ class BulkTransactionsClient(BaseBulkTransactionsClient):
             base_url = ""
 
         processed_items = [
-            self.preprocess_item(item, base_url) for item in items.items.values()
+            self.preprocess_item(item, base_url, items.method) for item in items.items.values()
         ]
 
         # not a great way to get the collection_id-- should be part of the method signature
