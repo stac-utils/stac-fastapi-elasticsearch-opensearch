9f54c9b09612f9002acc5f86544ade16a1f2aa33
jonhealy1 <jonathan.d.healy@gmail.com>
2024-02-05 19:44:25 +0800

    delete old test files

--- stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
@@ -9,12 +9,12 @@ from stac_fastapi.core.core import (
     TransactionsClient,
 )
 from stac_fastapi.core.extensions import QueryExtension
+from stac_fastapi.core.session import Session
 from stac_fastapi.elasticsearch.config import ElasticsearchSettings
 from stac_fastapi.elasticsearch.database_logic import (
     DatabaseLogic,
     create_collection_index,
 )
-from stac_fastapi.elasticsearch.session import Session
 from stac_fastapi.extensions.core import (
     ContextExtension,
     FieldsExtension,
