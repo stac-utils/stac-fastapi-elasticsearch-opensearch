d530cb8b41ae62ab3b8b420af99782954e31c573
jonhealy1 <jonathan.d.healy@gmail.com>
2024-02-06 12:54:09 +0800

    remove comment

--- stac_fastapi/tests/resources/test_item.py
+++ stac_fastapi/tests/resources/test_item.py
@@ -14,8 +14,6 @@ from pystac.utils import datetime_to_str
 
 from stac_fastapi.core.core import CoreClient
 from stac_fastapi.core.datetime_utils import now_to_rfc3339_str
-
-# from stac_fastapi.core.base_database_logic import BaseDatabaseLogic
 from stac_fastapi.types.core import LandingPageMixin
 
 from ..conftest import create_item, refresh_indices
