6b018985cef04d1439d97a8015680adbe5d30e63
Stijn Caerts <stijn.caerts@vito.be>
2024-03-12 15:01:17 +0100

    implement the use of index templates (#208)

---
+++ stac_fastapi/tests/database/test_database.py
@@ -0,0 +1,47 @@
+import os
+import uuid
+from copy import deepcopy
+
+import pytest
+
+from ..conftest import MockRequest, database
+
+if os.getenv("BACKEND", "elasticsearch").lower() == "opensearch":
+    from stac_fastapi.opensearch.database_logic import (
+        COLLECTIONS_INDEX,
+        ES_COLLECTIONS_MAPPINGS,
+        ES_ITEMS_MAPPINGS,
+        index_by_collection_id,
+    )
+else:
+    from stac_fastapi.elasticsearch.database_logic import (
+        COLLECTIONS_INDEX,
+        ES_COLLECTIONS_MAPPINGS,
+        ES_ITEMS_MAPPINGS,
+        index_by_collection_id,
+    )
+
+
+@pytest.mark.asyncio
+async def test_index_mapping_collections(ctx):
+    response = await database.client.indices.get_mapping(index=COLLECTIONS_INDEX)
+    actual_mappings = next(iter(response.body.values()))["mappings"]
+    assert (
+        actual_mappings["dynamic_templates"]
+        == ES_COLLECTIONS_MAPPINGS["dynamic_templates"]
+    )
+
+
+@pytest.mark.asyncio
+async def test_index_mapping_items(ctx, txn_client):
+    collection = deepcopy(ctx.collection)
+    collection["id"] = str(uuid.uuid4())
+    await txn_client.create_collection(collection, request=MockRequest)
+    response = await database.client.indices.get_mapping(
+        index=index_by_collection_id(collection["id"])
+    )
+    actual_mappings = next(iter(response.body.values()))["mappings"]
+    assert (
+        actual_mappings["dynamic_templates"] == ES_ITEMS_MAPPINGS["dynamic_templates"]
+    )
+    await txn_client.delete_collection(collection["id"])
