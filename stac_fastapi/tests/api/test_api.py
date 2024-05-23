c69c9e57d0ecf7dbf9180e5d45386a464156b5c3
rhysrevans3 <rhys.r.evans3@gmail.com>
2024-05-23 13:44:53 +0100

    Fixing datetime type issue.

--- stac_fastapi/tests/api/test_api.py
+++ stac_fastapi/tests/api/test_api.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import pytest
 
@@ -251,7 +251,9 @@ async def test_app_sort_extension_get_asc(app_client, txn_client, ctx):
 
     second_item = dict(first_item)
     second_item["id"] = "another-item"
-    another_item_date = first_item["properties"]["datetime"] - timedelta(days=1)
+    another_item_date = datetime.strptime(
+        first_item["properties"]["datetime"], "%Y-%m-%dT%H:%M:%SZ"
+    ) - timedelta(days=1)
     second_item["properties"]["datetime"] = another_item_date.isoformat().replace(
         "+00:00", "Z"
     )
@@ -271,7 +273,9 @@ async def test_app_sort_extension_get_desc(app_client, txn_client, ctx):
 
     second_item = dict(first_item)
     second_item["id"] = "another-item"
-    another_item_date = first_item["properties"]["datetime"] - timedelta(days=1)
+    another_item_date = datetime.strptime(
+        first_item["properties"]["datetime"], "%Y-%m-%dT%H:%M:%SZ"
+    ) - timedelta(days=1)
     second_item["properties"]["datetime"] = another_item_date.isoformat().replace(
         "+00:00", "Z"
     )
@@ -290,7 +294,9 @@ async def test_app_sort_extension_post_asc(app_client, txn_client, ctx):
 
     second_item = dict(first_item)
     second_item["id"] = "another-item"
-    another_item_date = first_item["properties"]["datetime"] - timedelta(days=1)
+    another_item_date = datetime.strptime(
+        first_item["properties"]["datetime"], "%Y-%m-%dT%H:%M:%SZ"
+    ) - timedelta(days=1)
     second_item["properties"]["datetime"] = another_item_date.isoformat().replace(
         "+00:00", "Z"
     )
@@ -313,7 +319,9 @@ async def test_app_sort_extension_post_desc(app_client, txn_client, ctx):
 
     second_item = dict(first_item)
     second_item["id"] = "another-item"
-    another_item_date = first_item["properties"]["datetime"] - timedelta(days=1)
+    another_item_date = datetime.strptime(
+        first_item["properties"]["datetime"], "%Y-%m-%dT%H:%M:%SZ"
+    ) - timedelta(days=1)
     second_item["properties"]["datetime"] = another_item_date.isoformat().replace(
         "+00:00", "Z"
     )
