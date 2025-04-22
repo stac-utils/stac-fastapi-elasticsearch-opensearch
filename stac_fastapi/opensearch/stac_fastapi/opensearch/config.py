f4e9c3f82368ba4a9dd15c737ade86c6f55f9b87
jonhealy1 <jonathan.d.healy@gmail.com>
2025-04-22 12:22:26 +0800

    ignore

--- stac_fastapi/opensearch/stac_fastapi/opensearch/config.py
+++ stac_fastapi/opensearch/stac_fastapi/opensearch/config.py
@@ -44,7 +44,7 @@ def _es_config() -> Dict[str, Any]:
         return config
 
     # Include SSL settings if using https
-    config["ssl_version"] = ssl.PROTOCOL_SSLv23  # type: ignore
+    config["ssl_version"] = ssl.PROTOCOL_SSLv23
     config["verify_certs"] = get_bool_env("ES_VERIFY_CERTS", default=True)
 
     # Include CA Certificates if verifying certs
