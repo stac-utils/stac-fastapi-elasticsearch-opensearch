2c0dc96ad3bfeda5c696d3f8b624890ab4c35553
Safak Yamali <safak@analyticsandsociety.com>
2026-02-09 14:12:48 -0600

    feat: Add centroid_geohex_grid_frequency to DEFAULT_AGGREGATIONS

--- stac_fastapi/sfeos_helpers/stac_fastapi/sfeos_helpers/aggregation/client.py
+++ stac_fastapi/sfeos_helpers/stac_fastapi/sfeos_helpers/aggregation/client.py
@@ -64,6 +64,11 @@ class EsAsyncBaseAggregationClient(AsyncBaseAggregationClient):
             "data_type": "frequency_distribution",
             "frequency_distribution_data_type": "string",
         },
+        {
+            "name": "centroid_geohex_grid_frequency",
+            "data_type": "frequency_distribution",
+            "frequency_distribution_data_type": "string",
+        },
     ]
 
     # Geo point aggregations
