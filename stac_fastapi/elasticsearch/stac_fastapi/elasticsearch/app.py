7d5e4fede24bea5cd11a65f95b8eae399ccd3d0a
jonhealy1 <jonathan.d.healy@gmail.com>
2026-04-16 11:47:48 +0800

    release v6.16.0

--- stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
+++ stac_fastapi/elasticsearch/stac_fastapi/elasticsearch/app.py
@@ -262,7 +262,7 @@ items_get_request_model = create_request_model(
 app_config = {
     "title": os.getenv("STAC_FASTAPI_TITLE", "stac-fastapi-elasticsearch"),
     "description": os.getenv("STAC_FASTAPI_DESCRIPTION", "stac-fastapi-elasticsearch"),
-    "api_version": os.getenv("STAC_FASTAPI_VERSION", "6.15.0"),
+    "api_version": os.getenv("STAC_FASTAPI_VERSION", "6.16.0"),
     "settings": settings,
     "extensions": extensions,
     "client": CoreClient(
