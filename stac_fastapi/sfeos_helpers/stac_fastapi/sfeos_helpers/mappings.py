1941e3969973db7f3cf9261a13f5cafe7b9735e2
rhysrevans3 <rhys.r.evans3@gmail.com>
2026-05-13 12:51:22 +0100

    Update item_indices.

--- stac_fastapi/sfeos_helpers/stac_fastapi/sfeos_helpers/mappings.py
+++ stac_fastapi/sfeos_helpers/stac_fastapi/sfeos_helpers/mappings.py
@@ -293,7 +293,7 @@ _ES_INDEX_NAME_UNSUPPORTED_CHARS_TABLE = str.maketrans(
     "", "", "".join(ES_INDEX_NAME_UNSUPPORTED_CHARS)
 )
 
-ITEM_INDICES = f"{ITEMS_INDEX_PREFIX}*"
+ITEM_INDICES = f"{ITEMS_ALIAS_PREFIX}*"
 
 DEFAULT_SORT = {
     "properties.datetime": {"order": "desc"},
