868afea29a8d9389af92ef02fc5bea3b291d0775
jonhealy1 <jonathan.d.healy@gmail.com>
2026-09-07 14:37:21 +0800

    ensure collection id set

--- stac_fastapi/core/stac_fastapi/core/core.py
+++ stac_fastapi/core/stac_fastapi/core/core.py
@@ -1378,6 +1378,8 @@ class TransactionsClient(AsyncBaseTransactionsClient):
 
         for feature in unique_features:
             try:
+                # Guarantee the collection field is set correctly (same as single-item path)
+                feature["collection"] = collection_id
                 prepped = bulk_client.preprocess_item(feature, base_url)
                 if prepped is not None:
                     processed_items.append(prepped)
